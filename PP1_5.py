79715a4b4a9b96653798b284e8e1df8b4740ca8d
Jinming-Chen-ICD2O1-01 <jinming.chen@student.tdsb.on.ca>
2024-11-06 09:54:43 -0500

    Update PP1_5.py

--- PP1_5.py
+++ PP1_5.py
@@ -1,21 +1,34 @@
-
+'''
+    Lesson: Typecasting
+    Author: Jinming Chen
+    Date Created: Nove 06 2024
+    Date Last Modified: Nove 06, 2024
+'''
 def q1():
   #Write Assignment code here
-
+# Ask the user to enter an integer, add 3 and output
+num= input("Input an integer: ")
+print(num + 3)
 def q2():
   #Write Assignment code here
-
+# Ask the user to enter a number, add 4 to the end, convert to a floating point number, add 2 and output
+num = input("Input a number: ")
+num = float(num + "4")
+print(num+ 2)
 def q3():
   #Write Assignment code here
-
+# Ask the user to enter a radius, calculate the area of ​​the circle and output
+num = float(input("Input a radius: "))
+print(3.14 * num * num)
 def q4():
   #Write Assignment code here
-
+# Ask the user to enter a number, convert to a floating point number, multiply by 12 and round down, then output the result
+numt = float(input("Input a number: "))
+print(int(num * 12))
 def q5():
   #Write Assignment code here
 
-#Comment this code out when running tests
-#Do not comment this out when running your program normally
+
 
 q1()
 q2()
